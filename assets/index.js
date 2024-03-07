d85fff9c97ab18b8e4e96cf8975b0a2019b2bf01
Tomas Roa <tomas.roa@gmail.com>
2024-03-06 23:14:23 -0300

    creacion de archivos, inicio de la logica del juego

---
+++ assets/index.js
@@ -0,0 +1,30 @@
+
+let nJuegos = parseInt(prompt("¿Cuantas veses deseas jugar?"));
+
+let opcionJ1 = prompt("Seleccione su jugada: piedra, papel o tijera").toLowerCase;;
+
+
+
+
+
+function juegoCachipun(nJuegos){
+
+    for (let i=0; i < nJuegos; i++){
+        let opcionJ1 = prompt("Seleccione su jugada: piedra, papel o tijera").toLowerCase;
+
+
+
+    };
+}
+
+
+function opcionPC () {
+    const opciones = ["piedra", "papel", "tijera"];
+    const aleatorio = Math.floor(Math.random()*3);
+    return opciones[aleatorio];
+
+}
+
+let opciones = ["piedra", "papel", "tijera"];
+
+
